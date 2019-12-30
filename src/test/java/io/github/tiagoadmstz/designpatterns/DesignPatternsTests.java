5f5e95aacd51ba92dda8da2ea7d9f0e93a7d8b68
Tiago D Teixeira <tiagoadmstz@yahoo.com.br>
2019-12-30 20:50:55 -0300

    30/12/2019

---
+++ src/test/java/io/github/tiagoadmstz/designpatterns/DesignPatternsTests.java
@@ -0,0 +1,12 @@
+package io.github.tiagoadmstz.designpatterns;
+
+import org.junit.jupiter.api.Test;
+
+public class DesignPatternsTests {
+
+    @Test
+    public void test(){
+        System.out.println("Teste");
+    }
+
+}
